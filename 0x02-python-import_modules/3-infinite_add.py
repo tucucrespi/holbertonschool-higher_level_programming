7037100982cab65a44e319f5d71534f61188b0be
tucucrespi <tucucrespi@hotmail.com>
2020-12-10 17:02:15 +0000

    add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 import sys
 if __name__ == "__main__":
-    a=0
-    if len(sys.argv) !=1:
+    a = 0
+    if len(sys.argv) != 1:
         for i in range(1, len(sys.argv)):
-            a+= int(sys.argv[i])
+            a + = int(sys.argv[i])
     print("{:d}".format(a))
