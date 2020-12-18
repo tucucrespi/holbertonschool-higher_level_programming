5196f9a22e144128861c40d8fd50d118f6215537
tucucrespi <tucucrespi@hotmail.com>
2020-12-18 02:03:36 +0000

    add

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary:
+        mx = 0
+        for i in a_dictionary:
+            if (mx < a_dictionary[i]):
+                mx = a_dictionary[i]
+                a = i
+        return (a)
+    else:
+        return (None)
