7d643b03647ae9a5e04849ce3a2bc30d5569611a
tucucrespi <tucucrespi@hotmail.com>
2021-03-27 20:40:57 +0000

    add

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Script that lists all states from the database hbtn_0e_0_usa"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    """Hago la conexión a la BD, obtengo un objeto db"""
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=argv[1], passwd=argv[2], db=argv[3])
+    """Obtengo un cursor para ejecutar las consultas"""
+    cur = db.cursor()
+    """Ejecuto consulta utilizando el cursor"""
+    cur.execute("SELECT * FROM states ORDER BY id")
+    for i in cur:
+        print(cur.fetchone())
+    cur.close()
+    db.close()
