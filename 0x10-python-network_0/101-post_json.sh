4300f192877aa8f2e0e34295538b28c436554984
tucucrespi <tucucrespi@hotmail.com>
2021-04-09 00:40:06 +0000

    add advanced

---
+++ 0x10-python-network_0/101-post_json.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Write a Bash script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
+curl -s -H "Content-Type: application/json" -X POST "$1" --data @"$2"
