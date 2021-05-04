1f17be5355ed7c0361481439483545a245aa6890
tucucrespi <tucucrespi@hotmail.com>
2021-05-04 21:50:34 +0000

    add tasks

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,20 +1,14 @@
 #!/usr/bin/node
+// makes get request for SW movie id
 const request = require('request');
-
-const url = process.argv[2];
-
-request(url, function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
-    let count = 0;
-    const dict = JSON.parse(body);
-    const movies = dict.results;
-    for (let i = 0; i < movies.length; i++) {
-      if (movies[i].characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
-        count += 1;
-      }
+const find = '/18/';
+request(process.argv[2], function (error, response, body) {
+  if (error) throw new Error(error);
+  let num = 0;
+  for (const film of JSON.parse(body).results) {
+    for (const character of film.characters) {
+      num += (character.includes(find) ? 1 : 0);
     }
-    console.log(count);
   }
+  console.log(num);
 });
